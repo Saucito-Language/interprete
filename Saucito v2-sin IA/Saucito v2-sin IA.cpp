10d4390a3315c63156ddb5920e426fd51b975ab2
jorgeivaneisaac-hub <jorgeivaneisaac@gmail.com>
2026-03-06 21:31:03 -0600

    Correccion de codigo

--- Saucito v2-sin IA/Saucito v2-sin IA.cpp
+++ Saucito v2-sin IA/Saucito v2-sin IA.cpp
@@ -293,7 +293,7 @@ private:
 
 int main() {
     // 1. Quitamos el while(!WindowShouldClose()) de aquí arriba.
-    system("clear");    
+    limpiarConsola();    
     std::string n;
     std::cout << "Que tal iniciaste saucito." << std::endl;
     std::cout << "Escribe el codigo a analizar : " << std::endl;
